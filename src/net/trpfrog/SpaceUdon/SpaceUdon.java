47231dc17a91a06f3ca148cb6d0a0943578bd046
つまみ <mtomo@me.com>
2020-12-04 19:04:04 +0900

    refactor: クラス名とパッケージ名を変更

--- src/net/trpfrog/SpaceUdon/SpaceUdon.java
+++
@@ -1,7 +0,0 @@
-package net.trpfrog.SpaceUdon;
-
-public class SpaceUdon {
-    public static void main(String[] args) {
-        System.out.println("Hello, Udon!");
-    }
-}
---
+++ src/net/trpfrog/medipro_game/MediproGame.java
@@ -0,0 +1,7 @@
+package net.trpfrog.medipro_game;
+
+public class MediproGame {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
