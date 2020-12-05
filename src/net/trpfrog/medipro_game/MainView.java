f5bb7a91c2feecd9654eddc0847a2ceac9f30307
つまみ <mtomo@me.com>
2020-12-05 22:56:59 +0900

    feat: MainViewにコントローラを登録できるように変更

--- src/net/trpfrog/medipro_game/MainView.java
+++ src/net/trpfrog/medipro_game/MainView.java
@@ -1,10 +1,12 @@
 package net.trpfrog.medipro_game;
 
+import net.trpfrog.medipro_game.scene.GameController;
 import net.trpfrog.medipro_game.scene.GameView;
 
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -26,7 +28,7 @@ public class MainView extends JFrame {
      * 唯一のMainViewのインスタンスを返します。
      * @return 唯一のMainViewのインスタンス
      */
-    public static MainView getView() {
+    public static MainView getInstance() {
         return mainView;
     }
 
@@ -36,7 +38,6 @@ public class MainView extends JFrame {
      */
     public void addGameView(GameView view) {
         activeViews.add(view);
-
     }
 
     /**
@@ -48,6 +49,37 @@ public class MainView extends JFrame {
         GameView view = activeViews.get(activeViews.size() - 1);
         activeViews.remove(activeViews.size() - 1);
         return view;
+    }
+
+    /**
+     * 指定したGameViewを削除します。
+     * @param view 削除するGameView
+     */
+    public void removeGameView(GameView view) {
+        // 通常は後ろの要素のみを削除するはずである
+        if(activeViews.get(activeViews.size() - 1) == view) {
+            popGameView();
+        } else {
+            activeViews.remove(view);
+        }
+    }
+
+    /**
+     * 指定したGameControllerをMainViewに登録します。
+     * @param controller 登録するコントローラ
+     */
+    public void addGameController(GameController controller) {
+        addMouseListener(controller);
+        addKeyListener(controller);
+    }
+
+    /**
+     * 指定したGameControllerをMainViewから削除します
+     * @param controller 削除するコントローラ
+     */
+    public void removeGameController(GameController controller) {
+        removeMouseListener(controller);
+        removeKeyListener(controller);
     }
 
     class MainPanel extends JPanel {
