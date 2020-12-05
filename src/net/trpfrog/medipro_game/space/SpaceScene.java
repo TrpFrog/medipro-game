3337800174cb34233242a5db3caaa4370202fb34
つまみ <mtomo@me.com>
2020-12-05 18:41:03 +0900

    feat: 宇宙空間移動に関する雛形を追加

---
+++ src/net/trpfrog/medipro_game/space/SpaceScene.java
@@ -0,0 +1,30 @@
+package net.trpfrog.medipro_game.space;
+
+import net.trpfrog.medipro_game.scene.*;
+
+public class SpaceScene implements GameScene {
+    @Override
+    public GameModel getModel() {
+        return null;
+    }
+
+    @Override
+    public GameView getView() {
+        return null;
+    }
+
+    @Override
+    public GameController getController() {
+        return null;
+    }
+
+    @Override
+    public void suspend() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+}
