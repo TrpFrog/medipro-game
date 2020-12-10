c783b9434e724cf293760a87a3d6e1039b0dd1fd
つまみ <mtomo@me.com>
2020-12-10 20:35:04 +0900

    feat: Starの追加
    星オブジェクトのクラスを追加しました。

---
+++ src/net/trpfrog/medipro_game/space/symbols/Star.java
@@ -0,0 +1,28 @@
+package net.trpfrog.medipro_game.space.symbols;
+
+import net.trpfrog.medipro_game.Drawable;
+
+import java.awt.*;
+
+/**
+ * 背景としての星の情報を持つクラス。
+ */
+public class Star extends Symbol {
+
+    public Star() {
+        this.setDrawer(new StarVisual());
+    }
+
+    public static Star getRandomStar() {
+        return new Star();
+    }
+
+    public static class StarVisual implements Drawable {
+
+        @Override
+        public void draw(Graphics2D g) {
+
+        }
+    }
+}
+
