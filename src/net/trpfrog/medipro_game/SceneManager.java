9e92904409e0c32b5c30b997a9fbf7675357fe2a
つまみ <mtomo@me.com>
2020-12-12 18:23:56 +0900

    feat: SceneManagerのDequeの変更を知らせるリスナを追加

--- src/net/trpfrog/medipro_game/SceneManager.java
+++ src/net/trpfrog/medipro_game/SceneManager.java
@@ -3,59 +3,76 @@ package net.trpfrog.medipro_game;
 import net.trpfrog.medipro_game.scene.GameScene;
 
 import java.util.*;
+import java.util.List;
 
 /**
  * ゲームのシーン管理をするクラス
  * @author つまみ
  */
 public class SceneManager {
-    private Stack<GameScene> stack = new Stack<>();
-    private final MainView mainView = MainView.getInstance();
+    private Deque<GameScene> deque = new ArrayDeque<>();
+    private static final SceneManager instance = new SceneManager();
 
-    private void activateScene(GameScene scene) {
-        mainView.addGameController(scene.getController());
-        mainView.addGameView(scene.getView());
-        scene.resume();
+    private SceneManager() {}
+
+    /**
+     * ゲーム内で唯一のSceneManagerを返します
+     * @return ゲーム内で唯一のSceneManager
+     */
+    public static SceneManager getInstance() {
+        return instance;
     }
 
-    private void inactivateScene(GameScene scene) {
-        scene.suspend();
-        mainView.removeGameController(scene.getController());
-        mainView.removeGameView(scene.getView());
+    // Listener関連
+    private List<SceneDequeListener> listeners = new LinkedList<>();
+
+    private void notifyDequeChanged() {
+        listeners.forEach(e -> e.sceneDequeChanged(deque));
+    }
+
+    /**
+     * SceneManagerのDequeが変更されたときに実行するListenerを登録します
+     * @param listener SceneManagerのDequeが変更されたときに実行するListener
+     */
+    public void addSceneDequeListener(SceneDequeListener listener) {
+        listeners.add(listener);
+    }
+
+    /**
+     * GameSceneの乗ったDequeを返します。
+     * @return GameSceneの乗ったDeque
+     */
+    protected Deque<GameScene> getDeque() {
+        return deque;
     }
 
     /**
      * 新たなゲームシーンをスタックに登録します。
-     * また、登録されたゲームシーンにこれに加えて次の3つの処理を行います。<br>
-     *  1. Viewにコントローラを登録。<br>
-     *  2. Viewに描画処理を登録。<br>
-     *  3. resume()を呼び出し、動作を再開させる。<br>
-     * また、1つ前に追加されたシーンには上と真逆の処理を行います。
      * @param scene 新しく追加されるシーン
      */
     public void push(GameScene scene) {
-        inactivateScene(stack.peek());
-        activateScene(scene);
-        stack.push(scene);
+        if(!deque.isEmpty()) {
+            deque.peekFirst().suspend();
+        }
+        scene.resume();
+        deque.addFirst(scene);
+        notifyDequeChanged();
     }
 
     /**
      * 最後に追加されたゲームシーンを取り出します。
-     * また、これに加えて次の3つの処理を行います。<br>
-     *  1. suspend()を呼び出し、動作を停止させる。<br>
-     *  2. Viewからコントローラを削除。<br>
-     *  3. Viewから描画処理を削除。<br>
      * また、スタックが空になった場合、ソフトウェアを終了します。
-     * 空でなかった場合は上の3つと真逆の処理をスタックの先頭に適用します。
      * @return 取り出されたゲームシーン
      */
     public GameScene pop() {
-        inactivateScene(stack.peek());
-        GameScene ret = stack.pop();
-        if(stack.isEmpty()) {
+        assert deque.peekFirst() != null;
+        deque.peekFirst().suspend();
+        GameScene ret = deque.removeFirst();
+        if(deque.isEmpty()) {
             System.exit(0);
         }
-        activateScene(stack.peek());
+        deque.peekFirst().resume();
+        notifyDequeChanged();
         return ret;
     }
 }
