6fb24a6bb89fc4ac70f1d6831f7999b97867afa6
つまみ <mtomo@me.com>
2020-12-12 18:28:47 +0900

    feat: mainメソッドから実行できるようにした

--- src/net/trpfrog/medipro_game/MediproGame.java
+++ src/net/trpfrog/medipro_game/MediproGame.java
@@ -1,7 +1,13 @@
 package net.trpfrog.medipro_game;
 
+import net.trpfrog.medipro_game.space.SpaceScene;
+
 public class MediproGame {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SceneManager sceneManager = SceneManager.getInstance();
+        MainView view = new MainView(sceneManager);
+        new MainController(sceneManager, view);
+
+        sceneManager.push(new SpaceScene());
     }
 }
