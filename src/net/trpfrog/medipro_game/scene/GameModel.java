ef53893b188b7bc04a93bd0f0b46c1c86388b342
つまみ <mtomo@me.com>
2020-12-16 15:35:30 +0900

    feat: GameModelがSymbolを管理するクラスを持つように変更

--- src/net/trpfrog/medipro_game/scene/GameModel.java
+++ src/net/trpfrog/medipro_game/scene/GameModel.java
@@ -1,9 +1,26 @@
 package net.trpfrog.medipro_game.scene;
 
+import net.trpfrog.medipro_game.symbol.Symbol;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
- * ゲームシーンのModelを定義するインタフェース。
- * ゲームシーンのModelは必ずこのインタフェースを実装してください。
+ * ゲームシーンのModelを定義する抽象クラス。
+ * ゲームシーンのModelは必ずこの抽象クラスを継承してください。
  * @author つまみ
  */
-public interface GameModel extends GameMVC {
+public abstract class GameModel implements GameMVC {
+
+    private List<Symbol> symbolsList = new LinkedList<>();
+
+    public List<Symbol> getSymbolsList() {
+        return symbolsList;
+    }
+
+    public void addSymbol(Symbol symbol) {
+        symbolsList.add(symbol);
+    }
 }
