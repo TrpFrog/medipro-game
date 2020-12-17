fbdd739384f3c0e3c179fba8498bb1dcb3f3d9d9
つまみ <mtomo@me.com>
2020-12-18 01:17:56 +0900

    feat: hasTransparencyの判断基準を変更

--- src/net/trpfrog/medipro_game/scene/GameView.java
+++ src/net/trpfrog/medipro_game/scene/GameView.java
@@ -20,11 +20,11 @@ public abstract class GameView extends JPanel implements GameMVC {
 
     /**
      * Viewが透明性を持つかを返します。
-     * デフォルトでは背景のアルファ値が255未満であることで判定します。
+     * デフォルトでは背景が透明であるかどうかで判定します。
      * これがtrueであると、SceneManagerのスタックで下にあるもののViewも一緒に描画されます。
      * @return Viewが透明性を持つか
      */
     public boolean hasTransparency() {
-        return getBackground().getAlpha() < 255;
+        return !isOpaque();
     }
 }
