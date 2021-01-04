1399ef8d679b823351cc542610e44e2f9d90c9d2
つまみ <mtomo@me.com>
2021-01-05 02:13:52 +0900

    docs: MiniGameSceneの説明を追加

--- src/net/trpfrog/medipro_game/mini_game/MiniGameScene.java
+++ src/net/trpfrog/medipro_game/mini_game/MiniGameScene.java
@@ -10,14 +10,26 @@ public class MiniGameScene extends GameScene {
     private String howToPlay = "";
     private String creatorName = "";
 
+    /**
+     * ミニゲームのタイトルを取得します。
+     * @return ミニゲームのタイトル
+     */
     public String getGameTitle() {
         return gameTitle;
     }
 
+    /**
+     * ミニゲームのタイトルを設定します。
+     * @param gameTitle ミニゲームのタイトル
+     */
     protected void setGameTitle(String gameTitle) {
         this.gameTitle = gameTitle;
     }
 
+    /**
+     * ミニゲームの説明を取得します。
+     * @return ミニゲームの説明
+     */
     public String getGameDescription() {
         return gameDescription;
     }
@@ -40,26 +52,51 @@ public class MiniGameScene extends GameScene {
         return ret;
     }
 
+    /**
+     * ミニゲームの説明を設定します。
+     * 可長変引数になっていて、引数ごとに改行されます。
+     * @param gameDescription ミニゲームの説明
+     */
     protected void setGameDescription(String... gameDescription) {
         this.gameDescription = generateLabelString(gameDescription);
     }
 
+    /**
+     * 作者の名前を取得します。
+     * @return 作者の名前
+     */
     public String getCreatorName() {
         return creatorName;
     }
 
+    /**
+     * 作者の名前を設定します。
+     * @param creatorName 作者の名前
+     */
     protected void setCreatorName(String creatorName) {
         this.creatorName = creatorName;
     }
 
+    /**
+     * 遊び方を返します。
+     * @return 遊び方
+     */
     public String getHowToPlay() {
         return howToPlay;
     }
 
+    /**
+     * 遊び方の説明文を設定します。
+     * 可長変引数になっていて、引数ごとに改行されます。
+     * @param howToPlay 遊び方の説明文
+     */
     public void setHowToPlay(String... howToPlay) {
         this.howToPlay = generateLabelString(howToPlay);
     }
 
+    /**
+     * 設定されたミニゲーム情報を元に説明用パネルを生成します。
+     */
     protected void makeDescriptionDialog() {
         var dialog = new DialogBackgroundScene(new MiniGameStartDialogPanel(this), true);
         addSubScene(dialog);
