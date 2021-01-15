71e0c9d1910de0ede31451a96677b729af694fcd
TomohiroIwase <iwase@psss.co.jp>
2021-01-15 23:03:05 +0900

    feat: ロケットをキー操作可能にした

--- src/net/trpfrog/medipro_game/space/SpaceController.java
+++ src/net/trpfrog/medipro_game/space/SpaceController.java
@@ -1,11 +1,13 @@
 package net.trpfrog.medipro_game.space;
 
+
 import net.trpfrog.medipro_game.scene.GameController;
+import net.trpfrog.medipro_game.space.symbols.Rocket;
 
 import java.awt.event.KeyEvent;
-import java.awt.event.MouseEvent;
+import java.awt.event.KeyListener;
 
-public class SpaceController extends GameController {
+public class SpaceController extends GameController implements KeyListener {
 
     private SpaceModel model;
     private SpaceView view;
@@ -13,7 +15,12 @@ public class SpaceController extends GameController {
     public SpaceController(SpaceModel model, SpaceView view) {
         super(model, view);
         this.model = model;
+
+        Rocket rocket = model.getRocket();
+        rocket.start(); // とりあえずC側でスタート
+
         this.view = view;
+        this.view.addKeyListener(this);
     }
 
     @Override
@@ -23,6 +30,43 @@ public class SpaceController extends GameController {
 
     @Override
     public void resume() {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        Rocket rocket = model.getRocket();
+        double currentSpeedPxPerSecond = rocket.getSpeedPxPerSecond();
+        double dSpeedPxPerSecond = 20.0; // とりあえず
+        double dAngleDegrees = 5.0; // とりあえず
+
+        switch (e.getKeyChar()){
+            // W: 加速
+            case 'w' -> {
+                if(500.0 <= currentSpeedPxPerSecond) return; // 上限速度の場合に離脱
+                currentSpeedPxPerSecond += dSpeedPxPerSecond;
+                rocket.setSpeedPxPerSecond(currentSpeedPxPerSecond);
+            }
+            // A: 左旋回
+            case 'a' -> rocket.turnAnticlockwiseDegrees(-dAngleDegrees);
+            // S: 減速
+            case 's' -> {
+                if(currentSpeedPxPerSecond <= 0) return; // 下限速度の場合に離脱
+                currentSpeedPxPerSecond -= dSpeedPxPerSecond;
+                rocket.setSpeedPxPerSecond(currentSpeedPxPerSecond);
+            }
+            // D: 右旋回
+            case 'd' -> rocket.turnAnticlockwiseDegrees(dAngleDegrees);
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
 
     }
 }
