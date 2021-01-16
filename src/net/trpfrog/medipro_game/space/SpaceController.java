24bbfa8d46f84aae08fd5b9aa4877fec79b75c45
hutinoatari <hutinoatari@gmail.com>
2021-01-16 19:39:07 +0900

    fix: コードクリーナー

--- src/net/trpfrog/medipro_game/space/SpaceController.java
+++ src/net/trpfrog/medipro_game/space/SpaceController.java
@@ -11,47 +11,55 @@ import java.awt.event.*;
 public class SpaceController extends GameController implements KeyListener, MouseMotionListener, MouseListener, MouseWheelListener {
     private SpaceModel model;
     private SpaceView view;
-    private boolean isUpperAngle;
     private Rocket rocket;
+    private Timer rotateTimerL, rotateTimerR, accelerateTimer, decelerateTimer, faceToGradientTimer;
+    private int fps, spf;
+    private boolean isUpperAngle;
 
     private void rotateTimerFunc(boolean isLeft){
         double dAngleDegrees = 5.0; // とりあえず
-        if(!this.isUpperAngle) dAngleDegrees *= -1;
+        if(!isUpperAngle) dAngleDegrees *= -1;
         if(isLeft) dAngleDegrees *= -1;
         rocket.turnAnticlockwiseDegrees(dAngleDegrees);
     }
-    private Timer rotateTimerL = new Timer(20, e -> rotateTimerFunc(true));
-    private Timer rotateTimerR = new Timer(20, e -> rotateTimerFunc(false));
-    private Timer accelerateTimer = new Timer(20, e -> rocket.accelerate(50.0));
-    private Timer decelerateTimer = new Timer(20, e -> rocket.accelerate(-25.0));
-
     private void faceToGradient(MouseEvent e){
         int mouseX = e.getX();
         int mouseY = e.getY();
-        double rocketX = rocket.getX();
-        double rocketY = rocket.getY();
-        double mapX = rocketX - view.getWidth()/2 + mouseX;
-        double mapY = rocketY - view.getHeight()/2 + mouseY;
-        double dx = mapX - rocketX;
-        double dy = mapY - rocketY;
+        double dx = mouseX - view.getWidth()/2;
+        double dy = mouseY - view.getHeight()/2;
 
         double toOtherAngleDegrees = Math.toDegrees(Math.atan2(dy, dx));
         double currentAngleDegrees = rocket.getAngleDegrees();
-        double rocketToOtherAngleDegrees = (toOtherAngleDegrees - currentAngleDegrees + 720) % 360;
+        double rocketToOtherAngleDegrees = ((toOtherAngleDegrees - currentAngleDegrees) % 360 + 360) % 360;
 
         double dAngleDegrees = 5.0;
-        if(Math.abs(rocketToOtherAngleDegrees) < 20.0) dAngleDegrees = 1.0;
-        if(Math.abs(rocketToOtherAngleDegrees) < 1.0) return;
+        double fastDAngleDegrees = 20.0;
+        double slowDAngleDegrees = 1.0;
+        if(Math.abs(rocketToOtherAngleDegrees) < fastDAngleDegrees) dAngleDegrees = slowDAngleDegrees;
+        if(Math.abs(rocketToOtherAngleDegrees) < slowDAngleDegrees) return;
 
         if(rocketToOtherAngleDegrees < 180) dAngleDegrees *= -1;
         rocket.turnClockwiseDegrees(dAngleDegrees);
     }
-    private Timer faceToGradientTimer;
+    private void moveDepth(int dir){
+        int currentDepth = rocket.getDepth();
+        int mapDepth = model.get3DMap().getDepth();
+        currentDepth += dir;
+        currentDepth = (currentDepth + mapDepth) % mapDepth;
+        rocket.setDepth(currentDepth);
+    }
 
     public SpaceController(SpaceModel model, SpaceView view) {
         super(model, view);
         this.model = model;
-        this.isUpperAngle = true;
+        isUpperAngle = true;
+        fps = 50;
+        spf = 1000 / fps;
+
+        rotateTimerL = new Timer(spf, e -> rotateTimerFunc(true));
+        rotateTimerR = new Timer(spf, e -> rotateTimerFunc(false));
+        accelerateTimer = new Timer(spf, e -> rocket.accelerate(50.0));
+        decelerateTimer = new Timer(spf, e -> rocket.accelerate(-25.0));
 
         rocket = model.getRocket();
         rocket.start(); // とりあえずC側でスタート
@@ -84,44 +92,33 @@ public class SpaceController extends GameController implements KeyListener, Mous
 
     @Override
     public void keyPressed(KeyEvent e) {
-        double dSpeedPxPerSecond = 50.0; // とりあえず
-
         switch (e.getKeyChar()){
             // W: 加速
-            case 'w' -> rocket.accelerate(dSpeedPxPerSecond);
+            case 'w' -> accelerateTimer.start();
             // A: 左旋回
             case 'a' -> rotateTimerL.start();
             // D: 右旋回
             case 'd' -> rotateTimerR.start();
             // Z: 上昇
-            case 'z' -> {
-                int currentDepth = rocket.getDepth();
-                int mapDepth = model.get3DMap().getDepth();
-                currentDepth += 1;
-                currentDepth = (currentDepth + mapDepth) % mapDepth;
-                rocket.setDepth(currentDepth);
-            }
+            case 'z' -> moveDepth(1);
             // X: 下降
-            case 'x' -> {
-                int currentDepth = rocket.getDepth();
-                int mapDepth = model.get3DMap().getDepth();
-                currentDepth -= 1;
-                currentDepth = (currentDepth + mapDepth) % mapDepth;
-                rocket.setDepth(currentDepth);
-            }
+            case 'x' -> moveDepth(-1);
         }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
         double currentAngleDegrees = rocket.getAngleDegrees();
+        double graceDegrees = 30.0;
+
         // 旋回方向の決定に少し猶予を持たせる
-        if(30<=currentAngleDegrees && currentAngleDegrees<=150){
+        if(graceDegrees<=currentAngleDegrees && currentAngleDegrees<=180-graceDegrees){
             this.isUpperAngle = false;
-        }else if(210<=currentAngleDegrees && currentAngleDegrees<=330){
+        }else if(180+graceDegrees<=currentAngleDegrees && currentAngleDegrees<=360-graceDegrees){
             this.isUpperAngle = true;
         }
 
+        accelerateTimer.stop();
         rotateTimerL.stop();
         rotateTimerR.stop();
     }
@@ -133,7 +130,7 @@ public class SpaceController extends GameController implements KeyListener, Mous
 
     @Override
     public void mousePressed(MouseEvent e) {
-        faceToGradientTimer = new Timer(20, o -> faceToGradient(e));
+        faceToGradientTimer = new Timer(spf, o -> faceToGradient(e));
         faceToGradientTimer.start();
         accelerateTimer.start();
     }
@@ -158,16 +155,16 @@ public class SpaceController extends GameController implements KeyListener, Mous
     public void mouseWheelMoved(MouseWheelEvent e) {
         int direction = e.getWheelRotation();
         int currentDepth = rocket.getDepth();
-        int mapDepth = model.get3DMap().getDepth();
+        int mapDepthLength = model.get3DMap().getDepth();
         currentDepth += direction;
-        currentDepth = (currentDepth + mapDepth) % mapDepth;
+        currentDepth = (currentDepth + mapDepthLength) % mapDepthLength;
         rocket.setDepth(currentDepth);
     }
 
     @Override
     public void mouseDragged(MouseEvent e) {
         faceToGradientTimer.stop();
-        faceToGradientTimer = new Timer(20, o -> faceToGradient(e));
+        faceToGradientTimer = new Timer(spf, o -> faceToGradient(e));
         faceToGradientTimer.start();
     }
 
