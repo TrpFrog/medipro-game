42b6c344f4cd0c3788f9ac24fa92a801a6eabd1d
hutinoatari <hutinoatari@gmail.com>
2021-01-16 20:20:43 +0900

    fix: moveDepth()内のmod計算を負数にならないよう書き直した、マウスでの階層移動をmoveDepth()に書き換えた

--- src/net/trpfrog/medipro_game/space/SpaceController.java
+++ src/net/trpfrog/medipro_game/space/SpaceController.java
@@ -45,7 +45,7 @@ public class SpaceController extends GameController implements KeyListener, Mous
         int currentDepth = rocket.getDepth();
         int mapDepth = model.get3DMap().getDepth();
         currentDepth += dz;
-        currentDepth = (currentDepth + mapDepth) % mapDepth;
+        currentDepth = (currentDepth % mapDepth + mapDepth) % mapDepth;
         rocket.setDepth(currentDepth);
     }
 
@@ -153,12 +153,8 @@ public class SpaceController extends GameController implements KeyListener, Mous
 
     @Override
     public void mouseWheelMoved(MouseWheelEvent e) {
-        int direction = e.getWheelRotation();
-        int currentDepth = rocket.getDepth();
-        int mapDepthLength = model.get3DMap().getDepth();
-        currentDepth += direction;
-        currentDepth = (currentDepth + mapDepthLength) % mapDepthLength;
-        rocket.setDepth(currentDepth);
+        int dz = e.getWheelRotation();
+        moveDepth(dz);
     }
 
     @Override
