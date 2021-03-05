389b635beb8dd583b40394c7923251de29d8a707
つまみ <dev@trpfrog.net>
2021-03-05 16:49:58 +0900

    feat: Moon's Work に高難易度版を追加

---
+++ src/net/trpfrog/medipro_game/mini_game/moons_work/BlackMoonsWorkScene.java
@@ -0,0 +1,11 @@
+package net.trpfrog.medipro_game.mini_game.moons_work;
+
+public class BlackMoonsWorkScene extends MoonsWorkScene {
+    public BlackMoonsWorkScene() {
+        super(2);
+        setGameTitle("BLACK Moon's Work");
+        setCreatorName("AtTAckEd by つまムーン");
+        setGameDescription("もっと！速く！");
+        makeDescriptionDialog();
+    }
+}
